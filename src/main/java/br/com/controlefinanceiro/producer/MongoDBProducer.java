77d9222cebe97a18885de3ef9ddeb1383cfad970
Javeson Anjos <javesonanjos@blumar.com.br>
2017-05-08 20:27:17 -0300

    Variavel de ambiente para MongoDB

--- src/main/java/br/com/controlefinanceiro/producer/MongoDBProducer.java
+++ src/main/java/br/com/controlefinanceiro/producer/MongoDBProducer.java
@@ -17,12 +17,12 @@ public class MongoDBProducer implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	private Datastore datastore;
-	private final String uri = "mongodb://javeson:FXMTmOU64YEsTiLA@cluster0-shard-00-00-xk8og.mongodb.net:27017,cluster0-shard-00-01-xk8og.mongodb.net:27017,cluster0-shard-00-02-xk8og.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+	//private final String uri = "";
 	
 	public MongoDBProducer() {
 		Morphia morphia = new Morphia();
 		morphia.mapPackage("br.com.controlefinanceiro");
-		MongoClientURI mongoClientURI = new MongoClientURI(uri);
+		MongoClientURI mongoClientURI = new MongoClientURI(System.getenv("MONGO_DB"));
 		datastore = morphia.createDatastore(new MongoClient(mongoClientURI), "test");
 	}
 
