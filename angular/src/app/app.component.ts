fa75231e90a510ae5822a6e6932d58e9d0061bc8
Javeson Anjos <javesonanjos@blumar.com.br>
2017-05-16 09:31:47 -0300

    Adicionado calendario

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-	constructor(){
-		localStorage.setItem('url', 'http://localhost:8080/controle-financeiro-ws/ws');
-	}
-}
+export class AppComponent {}
